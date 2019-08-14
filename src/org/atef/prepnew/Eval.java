e8a1bef912ac3b11ce404f11e182992aaa87a924
RoonBK <52622021+atefbk@users.noreply.github.com>
2019-08-14 18:14:57 +0200

    Update Eval.java

--- src/org/atef/prepnew/Eval.java
+++ src/org/atef/prepnew/Eval.java
@@ -28,7 +28,7 @@ public class Eval {
 		lstStr.add("Rawen");
 		lstStr.add("Saiida");
 		lstStr.add("Atef");
-		List<String> result = lstStr.stream().filter(e -> e.contains("Raw"))
+		List<String> result = lstStr.stream().filter(e -> e.contains("Ra"))
 									 .collect(Collectors.toList());
 		result.forEach(e -> System.out.println(e));
 		
