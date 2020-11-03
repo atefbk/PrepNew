59bfc1c81dc671c30f7503b8dd7716569981c2a0
RoonBK <52622021+atefbk@users.noreply.github.com>
2020-11-03 23:21:46 +0100

    Update PlusProche.java

--- src/org/atef/prepnew/PlusProche.java
+++ src/org/atef/prepnew/PlusProche.java
@@ -1,34 +1,27 @@
 package org.atef.prepnew;
 
 public class PlusProche {
-	/**
-	 * Etant donné un tableau d'entiers sans redondances, retourner l'élement la plus proche de zero
-	 * En cas de deux élements à même distance, retourner l'élement à valeur positive
-	 */
+	
+public static void main(String[] args) {
+        int tabb[] = {};
+        System.out.println(plusProcheDeZero(tabb));
 
-	public static void main(String[] args) {
-		
-		int[] tab = {9, -3, 7, 3};
-		System.out.println(plusProche(tab));
-	}
-	
-	public static int plusProche(int[] tab) {
-		if (tab.length == 1) return tab[0];
-		int result = tab[0];
-		for (int i = 1; i < tab.length; i++) {
-			if (valAbs(tab[i]) < valAbs(result))
-				result = tab[i];
-			if (valAbs(tab[i]) == valAbs(result)) {
-				if (tab[i] > result) result = tab[i];
-			}
-		}
-		return result;
-	}
-	
-	public static int valAbs(int x) {
-		return (x < 0) ? -x : x;
-	}
-	
+}
+    public static int plusProcheDeZero(int[] tab){
+        if (tab.length > 0) {
+            int result = tab[0];
+            if (tab.length>1){
+                for (int i = 1; i<tab.length; i++) {
+                    if ((Math.abs(tab[i]) < result)
+                            || ((tab[i] + result == 0) && (result < tab[i]))){
+                        result = tab[i];
+                    }
+                }
+            }
+            return result;
+        }
+        return 0;
+    }
 	
 
 }
