7ecc18be19d8c93bd3294fdfaaddcb2d99e2b2b8
RoonBK <52622021+atefbk@users.noreply.github.com>
2020-11-03 23:44:01 +0100

    Update PlusProche.java

--- src/org/atef/prepnew/PlusProche.java
+++ src/org/atef/prepnew/PlusProche.java
@@ -8,10 +8,13 @@ public static void main(String[] args) {
 
 }
     public static int plusProcheDeZero(int[] tab){
-        if (tab.length > 0) {
+        if ((tab.length > 0) && (tab[0] != 0)) {
             int result = tab[0];
             if (tab.length>1){
+                //System.out.println("Entrée dans la boucle !!!");
                 for (int i = 1; i<tab.length; i++) {
+                    //System.out.println("Parcouru: "+i);
+                    if(tab[i] == 0) return 0;
                     if ((Math.abs(tab[i]) < result)
                             || ((tab[i] + result == 0) && (result < tab[i]))){
                         result = tab[i];
